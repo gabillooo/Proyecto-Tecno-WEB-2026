cb1d7cdf6187b506da84b3633f915191385b4500
GabrielDelgado <gabriel.delgado.martinez@alumnos.uta.cl>
2026-09-22 00:47:04 -0300

    PrintOficial

---
+++ permisos-admin-sprint-1/src/app/features/admin/admin.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AdminRoutingModule } from './admin-routing.module';
+
+import { CatalogoPermisosComponent } from './pages/catalogo-permisos/catalogo-permisos.component';
+import { SolicitudesComponent } from './pages/solicitudes/solicitudes.component';
+import { EstadisticasComponent } from './pages/estadisticas/estadisticas.component';
+
+@NgModule({
+  declarations: [CatalogoPermisosComponent, SolicitudesComponent, EstadisticasComponent],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, AdminRoutingModule],
+})
+export class AdminModule {}
