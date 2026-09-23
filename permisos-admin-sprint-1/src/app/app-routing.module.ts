84bf3b4877108006326f98eedb475ff74489ef21
Benjamin <benja9989flores@gmail.com>
2026-09-22 21:39:59 -0300

    Ordenamiento de codigo
    Codigo mas limpio

--- permisos-admin-sprint-1/src/app/app-routing.module.ts
+++ permisos-admin-sprint-1/src/app/app-routing.module.ts
@@ -3,5 +3,35 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './core/guards/auth.guard';
 import { RoleGuard } from './core/guards/role.guard';
 import { RolUsuario } from './core/enums/estado-solicitud.enum';
-const routes: Routes = [{path:'',pathMatch:'full',redirectTo:'auth/login'},{path:'auth',loadChildren:()=>import('./auth/auth.module').then(m=>m.AuthModule)},{path:'admin',canActivate:[AuthGuard,RoleGuard],data:{rolesPermitidos:[RolUsuario.ADMINISTRADOR]},loadChildren:()=>import('./features/admin/admin.module').then(m=>m.AdminModule)},{path:'**',redirectTo:'auth/login'}];
-@NgModule({imports:[RouterModule.forRoot(routes)],exports:[RouterModule]}) export class AppRoutingModule {}
+
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'auth/login'
+  },
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./auth/auth.module').then((m) => m.AuthModule)
+  },
+  {
+    path: 'admin',
+    canActivate: [AuthGuard, RoleGuard],
+    data: {
+      rolesPermitidos: [RolUsuario.ADMINISTRADOR]
+    },
+    loadChildren: () =>
+      import('./features/admin/admin.module').then((m) => m.AdminModule)
+  },
+  {
+    path: '**',
+    redirectTo: 'auth/login'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
